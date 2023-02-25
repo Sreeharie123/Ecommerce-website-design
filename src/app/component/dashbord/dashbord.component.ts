48a190bc5b310fc9b6a5bc918926b1432803a0a3
Sreeharie123 <sreehariengoli@gmail.com>
2023-02-25 18:44:36 +0530

    design change

--- src/app/component/dashbord/dashbord.component.ts
+++ src/app/component/dashbord/dashbord.component.ts
@@ -2,39 +2,63 @@ import { Component } from '@angular/core';
 import { fromFetch } from 'rxjs/fetch';
 import { switchMap, of, catchError } from 'rxjs';
 
-
 @Component({
   selector: 'app-dashbord',
   templateUrl: './dashbord.component.html',
-  styleUrls: ['./dashbord.component.scss']
+  styleUrls: ['./dashbord.component.scss'],
 })
 export class DashbordComponent {
-  date:name[]=[
-    {date:13,hour:"Hour"},
-    {date:34,hour:"Min"},
-    {date:56,hour:"Sec"},
-  ]
+  date: name[] = [
+    { date: 13, hour: 'Hour' },
+    { date: 34, hour: 'Min' },
+    { date: 56, hour: 'Sec' },
+  ];
 
-  items:any=[];
-  data$ = fromFetch('https://dummyjson.com/products').pipe(
-  switchMap(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return of({ error: true, message: `Error ${ response.status }` });
-    }
-  }),
-  catchError(err => {
-    console.error(err);
-    return of({ error: true, message: err.message })
-  })
-).subscribe((data)=>{
- console.log(data);
- this.items=data.products
-} )
+  items: any = [];
+  items2: any = [];
+
+  data$ = fromFetch('https://dummyjson.com/products')
+    .pipe(
+      switchMap((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return of({ error: true, message: `Error ${response.status}` });
+        }
+      }),
+      catchError((err) => {
+        console.error(err);
+        return of({ error: true, message: err.message });
+      })
+    )
+    .subscribe((data) => {
+      // console.log(data);
+      this.items = data.products;
+    });
+
+
+  data2$ = fromFetch("https://dummyjson.com/products/1")
+    .pipe(
+      switchMap((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return of({ error: true, message: `Error ${res.status}` });
+        }
+      }),
+      catchError((err) => {
+        console.error(err);
+        return of({ error: true, message: err.message });
+      })
+    )
+    .subscribe((data) => {
+      console.log(data);
+      this.items2 = data;
+    });
+
 
 }
-interface name{
-  date:number,
-  hour:string
+interface name {
+  date: number;
+  hour: string;
 }
